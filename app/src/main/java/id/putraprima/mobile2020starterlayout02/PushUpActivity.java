feaebeafe593a9d399db755a03f6a7f2db126752
rizkyputrapb <rizkyputrapb@gmail.com>
2020-09-16 21:45:45 +0700

    finished project. adding proximity sensor and design

--- app/src/main/java/id/putraprima/mobile2020starterlayout02/PushUpActivity.java
+++ app/src/main/java/id/putraprima/mobile2020starterlayout02/PushUpActivity.java
@@ -2,13 +2,58 @@ package id.putraprima.mobile2020starterlayout02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PushUpActivity extends AppCompatActivity {
+public class PushUpActivity extends AppCompatActivity implements SensorEventListener {
+    static int count = 0;
+    private SensorManager mSensorManager;
+    private Sensor mProximity;
+    private static final int SENSOR_SENSITIVITY = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push_up);
+        final TextView counter = findViewById(R.id.txtPushupCount);
+        counter.setText(String.valueOf(count));
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        final TextView counter = findViewById(R.id.txtPushupCount);
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            if (event.values[0] >= -SENSOR_SENSITIVITY && event.values[0] <= SENSOR_SENSITIVITY) {
+                counter.setText(String.valueOf(count++));
+            } else {
+                counter.setText(String.valueOf(count));
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 }
